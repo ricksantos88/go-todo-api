52bee94e3f4c42eb238aa32aceb395f6cdacb8ad
Wendel Santos <wendel.santos@consultores.rnp.br>
2024-01-29 22:16:25 -0300

    config air for dev mod

---
+++ router/opening-routes.go
@@ -0,0 +1,40 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializeOpeningRoutes(router *gin.RouterGroup) {
+
+	v1 := router
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET opening",
+			})
+		})
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST opening",
+			})
+		})
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT opening",
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE opening",
+			})
+		})
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET openings",
+			})
+		})
+	}
+
+}
--- router/router.go
+++ router/router.go
@@ -3,11 +3,13 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	// Initialize router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	var openingRouter = router.Group("/api/v1")
+
+	// Initialize opening routes
+	initializeOpeningRoutes(openingRouter)
+
+	// Run the server
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
