48fb13bf6929d59df608e60d09beeb27c950bf6c
Wendel Santos <wendel.santos@consultores.rnp.br>
2024-01-29 22:17:00 -0300

    feat: add routes logic

--- router/opening-routes.go
+++ router/opening-routes.go
@@ -15,21 +15,25 @@ func initializeOpeningRoutes(router *gin.RouterGroup) {
 				"msg": "GET opening",
 			})
 		})
+
 		v1.POST("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "POST opening",
 			})
 		})
+
 		v1.PUT("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "PUT opening",
 			})
 		})
+
 		v1.DELETE("/opening", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "DELETE opening",
 			})
 		})
+
 		v1.GET("/openings", func(ctx *gin.Context) {
 			ctx.JSON(http.StatusOK, gin.H{
 				"msg": "GET openings",
